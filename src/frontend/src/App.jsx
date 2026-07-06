530e2e74b2aa97290bfb6da04d111948bf76161b
IhnatVyhuliar <ihnat.vyhuliar@gmail.com>
2026-07-06 16:21:53 +0200

    feat(views): add NightView and wire /night route

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainView } from '../views/MainView';
+import { NightView } from '../views/NightView';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainView />} />
+        <Route path="/night" element={<NightView />} />
       </Routes>
     </BrowserRouter>
   );
