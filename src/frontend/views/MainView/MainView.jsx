e6a913e55b315617f09dec9088f78afcb8c99170
Maciek <maciejlazarczyk0@gmail.com>
2026-07-07 12:01:36 +0200

    modified:   src/frontend/styles/globals.css 	modified:   src/frontend/views/MainView/MainView.jsx

--- src/frontend/views/MainView/MainView.jsx
+++ src/frontend/views/MainView/MainView.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { PatientViewer } from '../../components/PatientViewer';
+import { Model3D, Model3DProvider } from '../../components/3DModule';
 import styles from './MainView.module.css';
 
 export function MainView() {
   return (
     <div className={styles.layout}>
       <PatientViewer />
+      {/* TEMPORARY: manual verification of Model3D, remove when told to */}
+      <Model3DProvider url="/3DModels/FinalBaseMesh.obj">
+        <Model3D />
+      </Model3DProvider>
       <div className={styles.content}>Main View</div>
     </div>
   );
