8518bcc6e244a8d1ba5e76fc20135dd3e7ec2e7a
JJGebel <mikeboss5555@gmail.com>
2026-07-07 15:24:38 +0200

    "WIP4"

--- src/frontend/components/TableFol/Table.jsx
+++ src/frontend/components/TableFol/Table.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Table.module.css';
 import { TabElem } from './TabElemFol';
+import { Diagnose } from './DiagnoseFol'
 
 export function Table({ children, className = '', ...rest }) {
   const rootClassName = className ? `${styles.table} ${className}` : styles.table;
-
+  
   return (
     <div className={rootClassName} {...rest}>
       <TabElem />
+      <Diagnose />
       {children}
     </div>
   );
